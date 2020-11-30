20bd1916a3a671427a15a339abcb97a1f8f429c7
William Tong <willxtong@gmail.com>
2020-11-30 12:48:09 -0500

    implement trigger method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'John Doe', age: 10 });
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('change1');
+});
+user.on('change', () => {
+  console.log('change2');
+});
+user.on('save', () => {
+  console.log('save');
+});
 
-console.log(user);
+user.trigger('save');
--- src/models/User.ts
+++ src/models/User.ts
@@ -22,4 +22,11 @@ export class User {
     if (!this.events[eventName]) this.events[eventName] = [];
     this.events[eventName].push(callback);
   }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((callback) => callback());
+  }
 }
